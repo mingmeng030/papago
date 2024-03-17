412c30ba4eb0fb82ce88e4772af068808d9b1fc7
mingmeng030 <djwls_0716@naver.com>
2024-03-17 18:09:35 +0900

    add : card component style

--- src/component/card/SrcCard.js
+++ src/component/card/SrcCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 
 import arrow from "../../assets/arrow-south.svg";
 import copy from "../../assets/copy.png";
@@ -23,40 +23,66 @@ const SrcCard = ({ sourceLan, targetLan }) => {
     { name: "독일어", code: "de" },
   ];
 
+  const [isFocus, setIsFocus] = useState(false);
+  const textarea = useRef();
+
+  const onFocusTextArea = (event) => {
+    setIsFocus(true);
+    document.querySelector(".card-wrapper").classList.add("focus");
+  };
+
+  const onBlurTextArea = (event) => {
+    setIsFocus(false);
+    document.querySelector(".card-wrapper").classList.remove("focus");
+  };
+
+  const handleResizeHeight = () => {
+    textarea.current.style.height = "auto";
+    textarea.current.style.height = textarea.current.scrollHeight + "px";
+  };
+
   return (
     <div className="card src">
-      <div className="card-top src">
-        <div className="card-src-btn-container src">
-          <button className="card-srcLan-btn src">{sourceLan}</button>
-          <button className="card-arrow-btn src">
-            <img src={arrow} />
+      <div className="card-wrapper">
+        <div className="card-top src">
+          <div className="card-src-btn-container src">
+            <button className="card-srcLan-btn src">{sourceLan}</button>
+            <button className="card-arrow-btn src">
+              <img src={arrow} />
+            </button>
+          </div>
+          <button className="card-src-exchange-btn">
+            <img src={exchange}></img>
+          </button>
+          <div className="card-tg-btn-container src">
+            <button className="card-tgLan-btn src">{targetLan}</button>
+            <button className="card-arrow-btn src">
+              <img src={arrow} />
+            </button>
+          </div>
+        </div>
+        <div className="card-mid src">
+          <textarea
+            ref={textarea}
+            rows={1}
+            onFocus={onFocusTextArea}
+            onBlur={onBlurTextArea}
+            onChange={handleResizeHeight}
+            className="card-mid-textarea"
+            placeholder="번역할 내용을 입력하세요"
+          ></textarea>
+        </div>
+        <div className="card-bottom src ">
+          <div className="card-bottom-btn-container src">
+            <button className="card-bottom-copy-btn src">
+              <img src={copy} />
+            </button>
+          </div>
+          <button className="card-bottom-translate-btn">
+            <span className="translate-btn long">번역하기</span>
+            <span className="translate-btn short">번역</span>
           </button>
         </div>
-        <button className="card-src-exchange-btn">
-          <img src={exchange}></img>
-        </button>
-        <div className="card-tg-btn-container src">
-          <button className="card-tgLan-btn src">{targetLan}</button>
-          <button className="card-arrow-btn src">
-            <img src={arrow} />
-          </button>
-        </div>
-      </div>
-
-      <div className="card-mid src">
-        <textarea placeholder="번역할 내용을 입력하세요"></textarea>
-      </div>
-
-      <div className="card-bottom src">
-        <div className="card-bottom-btn-container src">
-          <button className="card-bottom-copy-btn src">
-            <img src={copy} />
-          </button>
-        </div>
-        <button className="card-bottom-translate-btn">
-          <span className="translate-btn long">번역하기</span>
-          <span className="translate-btn short">번역</span>
-        </button>
       </div>
     </div>
   );
--- src/component/card/TgtCard.js
+++ src/component/card/TgtCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import arrow from "../../assets/arrow-south.svg";
 import copy from "../../assets/copy.png";
@@ -7,25 +7,30 @@ import "../../styles/components/card.scss";
 import "../../styles/components/tgtCard.scss";
 
 const TgtCard = ({ targetLan }) => {
+  const [result, setResult] = useState("");
   return (
     <div className="card tgt">
-      <div className="card-top tgt">
-        <div className="card-tg-btn-container tgt">
-          <button className="card-tgLan-btn tgt">{targetLan}</button>
-          <button className="card-arrow-btn tgt">
-            <img src={arrow} />
-          </button>
+      <div className="card-wrapper">
+        <div className="card-top tgt">
+          <div className="card-tg-btn-container tgt">
+            <button className="card-tgLan-btn tgt">{targetLan}</button>
+            <button className="card-arrow-btn tgt">
+              <img src={arrow} />
+            </button>
+          </div>
         </div>
-      </div>
-      <div className="card-mid tgt"></div>
-      <div className="card-bottom tgt">
-        <div className="card-bottom btn-container tgt">
-          <button className="card-bottom-copy-btn tgt">
-            <img src={copy} />
-          </button>
-          <button className="card-bottom-share-btn tgt">
-            <img src={share} />
-          </button>
+        <div className="card-mid tgt">
+          <p className="card-mid-content">{result}</p>
+        </div>
+        <div className="card-bottom tgt">
+          <div className="card-bottom-btn-container tgt">
+            <button className="card-bottom-copy-btn tgt">
+              <img src={copy} />
+            </button>
+            <button className="card-bottom-share-btn tgt">
+              <img src={share} />
+            </button>
+          </div>
         </div>
       </div>
     </div>
